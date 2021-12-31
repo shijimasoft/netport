b02d80f77f5975b5c2afdca9f42df6e81112b5cc
shijimasoft <shijimasoft@gmail.com>
2021-12-31 14:24:25 +0100

    changelog v0.3 * NetPort can now be invoked with additional terminal parameters, please see [README](https://github.com/shijimasoft/netport/blob/main/README.md) * Some GUI and code improvements * Windows will no longer show the terminal along with GUI * Linux x86 binary added 🐧

--- README.md
+++ README.md
@@ -1,24 +1,31 @@
 # NetPort
 
+<center>
+
 ![license](https://img.shields.io/badge/license-Apache%202-green)
+![version](https://img.shields.io/badge/version-0.3-blueviolet)
 ![win64](https://img.shields.io/badge/windows-release64-informational)
+![linux64](https://img.shields.io/badge/linux-release64-orange)
 ![macos64](https://img.shields.io/badge/macOS-release64-magenta)
 
 *A GUI address port checker written in Rust*
 
-<p align="center"><img src="https://i.ibb.co/SxdTX5j/netport.png"></p>
+<img src="https://i.ibb.co/SxdTX5j/netport.png">
+</center>
 
 ## Install 📦
 
-*Windows and macOS binary files can be found in [Release](https://github.com/shijimasoft/netport/releases) section*
+*Windows, Linux and macOS binary files can be found in [Releases](https://github.com/shijimasoft/netport/releases) section*
 
 <br>
 
 <u> **Compile from Source Code** </u>
 
-No external dependencies are required on Windows and macOS.
+`cmake`, `git` and `g++` are required on all OS.
 
-on Linux you should install:
+No external dependencies are required on *Windows* and *macOS*,
+
+on *Linux* you should install:
 
 **Debian**
 `libx11-dev libxext-dev libxft-dev libxinerama-dev libxcursor-dev libxrender-dev libxfixes-dev libpango1.0-dev libgl1-mesa-dev libglu1-mesa-dev`
@@ -31,3 +38,21 @@ on Linux you should install:
 *Build binary with Rust compiler:*
 
 `cargo build --release`
+
+<br>
+
+## Terminal usage ⚡️
+
+NetPort GUI can be easily started with:
+
+`netport.exe` on Windows
+
+`./netport` on Linux/macOS
+
+It can also be invoked with parameters:
+
+`netport [address]` <u>Address</u> should have this format > **IP:PORT**  ( e.g. `127.0.0.1:80` )
+
+You can also specify the connection timeout:
+
+`netport [address] -t [time]` <u>Time</u> is expressed in seconds (Default is `8` seconds)
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#![windows_subsystem = "windows"]
+
 use std::thread;
 use std::net::{SocketAddr, TcpStream};
 use std::time::Duration;
 use fltk::{app, prelude::*, window::Window, frame::Frame, enums::FrameType, input::Input, button::Button};
 
 fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    let mut scan: bool = false;
+    let mut timeout: u64 = 8;
+
     let netport = app::App::default().with_scheme(app::AppScheme::Gtk);
     let mut window = Window::default()
         .with_size(300, 180)
@@ -17,19 +23,19 @@ fn main() {
     let _ip_label = Frame::new(125, 20, 50, 8, "IP address:");
 
     // IP dots
-    let _dot1_label = Frame::new(105, 54, 5, 5, ".");
-    let _dot2_label = Frame::new(148, 54, 5, 5, ".");
-    let _dot3_label = Frame::new(191, 54, 5, 5, ".");
+    let _dot_labels: [Frame;3] = [
+        Frame::new(105, 54, 5, 5, "."),
+        Frame::new(148, 54, 5, 5, "."),
+        Frame::new(191, 54, 5, 5, ".")
+    ];
 
     // Byte fields
-    let mut first_txt = Input::new(69, 40, 34, 24, "");
-    first_txt.set_maximum_size(3);
-    let mut second_txt = Input::new(112, 40, 34, 24, "");
-    second_txt.set_maximum_size(3);
-    let mut third_txt = Input::new(155, 40, 34, 24, "");
-    third_txt.set_maximum_size(3);
-    let mut fourth_txt = Input::new(198, 40, 34, 24, "");
-    fourth_txt.set_maximum_size(3);
+    let mut bytes_txt: [Input;4] = [
+        Input::new(69, 40, 34, 24, ""),
+        Input::new(112, 40, 34, 24, ""),
+        Input::new(155, 40, 34, 24, ""),
+        Input::new(198, 40, 34, 24, "")
+    ];
 
     /* PORT PANEL */
     let mut port_panel = Frame::new(15,90,100,70,"");
@@ -46,35 +52,61 @@ fn main() {
     let mut status_panel = Frame::new(125,90,160,70,"");
     status_panel.set_frame(FrameType::GtkDownFrame);
 
-    let mut status_label = Frame::new(158, 97, 90, 14, "Status");
-    let mut address_label = Frame::new(140, 102, 130, 50, "");
+    let mut status_label = Frame::new(154, 100, 100, 14, "Status");
+    let mut address_label = Frame::new(140, 105, 130, 50, "");
 
-    check_btn.set_callback(move |_| {
-        let bytes: [String;4] = [first_txt.value(), second_txt.value(), third_txt.value(), fourth_txt.value()];
+    /* ARGS handling (Address and Timeout) */
+    if args.len() >= 2 && args[1].parse::<SocketAddr>().is_ok() 
+    {
+        let address: Vec<&str> = args[1].split(":").collect();
+        let bytes: Vec<&str> = address[0].split(".").collect();
+
+        for i in 0..4
+            {bytes_txt[i].set_value(bytes[i])}
+        port_txt.set_value(address[1]);
+
+        scan = true;
+    }
+
+    if args.len() == 4 && args[2] == "-t" && args[3].parse::<u64>().is_ok() 
+        {timeout = args[3].parse::<u64>().unwrap()}
+
+    /* SCAN */
+    check_btn.set_callback(move |check_btn| {
+        check_btn.deactivate();
+
+        let bytes: Vec<String> = bytes_txt.iter().map(|byte_txt| byte_txt.value()).collect();
         let abort: bool;
 
         let address: String = format!("{}:{}", bytes.join("."), port_txt.value());
         if address.parse::<SocketAddr>().is_err() {abort = true} else {abort=false}
 
         if !abort {
+            address_label.set_pos(140, 105);
             address_label.set_label(&address);
             status_label.set_label("Scanning...");
 
+            // Pointers copy
             let mut status_label = status_label.clone();
+            let mut check_btn = check_btn.clone();
 
             thread::spawn(move || {
-                if TcpStream::connect_timeout(&address.parse::<SocketAddr>().unwrap(), Duration::from_secs(8)).is_ok() 
+                if TcpStream::connect_timeout(&address.parse::<SocketAddr>().unwrap(), Duration::from_secs(timeout)).is_ok() 
                     {status_label.set_label("Status: Open")} 
                 else 
                     {status_label.set_label("Status: Closed")}
+                check_btn.activate();
             });
 
         } else {
             status_label.set_label("");
-            address_label.set_label("Invalid IP/Port")
+            address_label.set_pos(139, 99);
+            address_label.set_label("Invalid IP/Port");
+            check_btn.activate();
         }
     });
 
+    if scan {check_btn.do_callback()}
     window.show();
     netport.run().unwrap();
 }
